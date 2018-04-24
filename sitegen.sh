56a1a11627b7bc138568193136d9bd60ad59dcd9
Sagar <sagar.writeme@gmail.com>
2018-04-24 14:26:16 +0530

    Basic AMP page

--- sitegen.sh
+++ sitegen.sh
@@ -3,3 +3,4 @@
 python pagerender.py finance/index.md blogindex.html > finance/index.html
 
 find finance -mindepth 1 -maxdepth 1 -type d -exec sh -c "python pagerender.py {}/index.md blog.html > {}/index.html" \;
+find finance -mindepth 1 -maxdepth 1 -type d -exec sh -c "python pagerender.py {}/index.md blogamp.html > {}/amp/index.html" \;
